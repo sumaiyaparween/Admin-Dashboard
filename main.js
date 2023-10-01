a19676287f0463e39faf5067322b4a9573178e77
sumaiyaparween <sumaiyaparween130@gmail.com>
2023-10-01 13:37:35 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+conts.one=document.queryselector(".one");
+ conts.one =document.queryselector(".two");
+ conts.one= document.queryselector(".three");
+ conts.one =document.queryselector(".four");
+ conts.one=document.queryselector(".five");
+
+
+ one,onclick=function(){
+    one.classlist.add("active");
+    two.classlist.remove("active");
+    three.classlist.remove("active");
+    four.classlist.remove("active");
+    five.classlist.remove("active");
+ }
+ two,onclick=function(){
+    one.classlist.add("active");
+    two.classlist.add("active");
+    three.classlist.remove("active");
+    four.classlist.remove("active");
+    five.classlist.remove("active");
+ }
+ three,onclick=function(){
+    one.classlist.add("active");
+    two.classlist.add("active");
+    three.classlist.add("active");
+    four.classlist.remove("active");
+    five.classlist.remove("active");
+ }
+ four,onclick=function(){
+    one.classlist.add("active");
+    two.classlist.add("active");
+    three.classlist.add("active");
+    four.classlist.add("active");
+    five.classlist.remove("active");
+ }
+ five,onclick=function(){
+    one.classlist.add("active");
+    two.classlist.add("active");
+    three.classlist.add("active");
+    four.classlist.add("active");
+    five.classlist.add("active");
+}
